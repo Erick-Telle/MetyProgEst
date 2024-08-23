4ea6112251fd06e864493ba566c41089b6f75cb8
Erick Tellez <ebguido@uamv.edu.ni>
2024-08-23 01:18:01 -0600

    Hola mundo mejorado

--- EjerciciosSemana1/Program1.cs
+++ EjerciciosSemana1/Program1.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace EjerciciosSemana1
 {
-    internal class Program
+    internal class Program1
     {
         static void Main(string[] args)
         {
+            //Programa que manda a imprimir en la consola "Hola Mundo"
+            Console.WriteLine("Hola mundoo");
         }
     }
 }
